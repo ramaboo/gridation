886996b1314377ac95a527050d541414d4f4dfa8
David Singer <david@ramaboo.com>
2011-02-26 23:57:53 -0800

    first commit

--- lib/gridation.rb
+++ lib/gridation.rb
@@ -1,4 +1,7 @@
 require 'compass'
-Compass::Frameworks.register('gridation', :path => "#{File.dirname(__FILE__)}/..")
+extension_path = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+Compass::Frameworks.register('gridation', :path => extension_path)
 
 
+module Gridation
+end
